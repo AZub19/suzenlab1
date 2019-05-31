5f759c14dda060a72f6294a273ed6e31610ad798
AZub19 <AZub19@outlook.com>
2019-05-31 21:19:33 +0300

    Suzen23 solution

---
+++ Suzen23.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd destination
+mv ngnix1.log ngnix1.log.back 
+mv ngnix2.log ngnix2.log.back
+mv ngnix3.log ngnix3.log.back
+mv ngnix4.log ngnix4.log.back
+mv ngnix5.log ngnix5.log.back
+cd ..
+mv *log /home/suzen/destination
+cd ..
+ls -a
